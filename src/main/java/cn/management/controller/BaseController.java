3be47ff152328f1a9a264007e29f7847905c6bda
ZhouJiaKai <zhoujk@pvc123.com>
2018-02-24 14:35:38 +0800

    refactor:修改BaseController

--- src/main/java/cn/management/controller/BaseController.java
+++ src/main/java/cn/management/controller/BaseController.java
@@ -1,9 +1,15 @@
 package cn.management.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.github.pagehelper.PageInfo;
+
 import cn.management.domain.BaseEntity;
+import cn.management.enums.ResultEnum;
 import cn.management.service.BaseService;
+import cn.management.util.Result;
 import tk.mybatis.mapper.entity.Example;
 
 /**
@@ -20,7 +26,24 @@ public class BaseController<SERVICE extends BaseService, M extends BaseEntity> {
      * 添加查询
      */
     private Example example;
-
+    
+    /**
+     * 获取数据列表
+     * @param page
+     * @return
+     */
+    protected Result list(Integer page) {
+        if (null == getExample().getOrderByClause()) {
+            getExample().setOrderByClause("id DESC");   //设置默认排序方式
+        };
+        List<M> list = service.getItemsByPage(getExample(), page, getPageSize());
+        if (list == null || list.size() == 0) {
+            return new Result(ResultEnum.NO_RECORDS);
+        }
+        PageInfo<M> pageInfo = new PageInfo<M>(list);
+        return new Result(ResultEnum.SUCCESS, pageInfo.getList(), pageInfo.getSize(), pageInfo.getPageNum(), getPageSize());
+    }
+    
     public Example getExample() {
         return example;
     }
