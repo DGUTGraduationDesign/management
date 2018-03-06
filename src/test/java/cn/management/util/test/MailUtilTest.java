4722eea3c324cddffb81eb8e6810daee2b5ae051
ZhouJiaKai <zhoujk@pvc123.com>
2018-03-06 17:17:18 +0800

    test:添加邮件发送测试类、短信发送测试类

---
+++ src/test/java/cn/management/util/test/MailUtilTest.java
@@ -0,0 +1,47 @@
+package cn.management.util.test;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import cn.management.util.SmsUtil;
+import cn.management.util.sms.SmsMultiSenderResult;
+
+/**
+ * MailUtil测试类
+ * @author ZhouJiaKai
+ * @date 2018-03-06
+ */
+public class MailUtilTest {
+    
+	/**
+	 * 使用模版群发
+	 */
+    @Test
+    public void sendMultiMessageWithParamTest() {
+        SmsMultiSenderResult smsMultiSenderResult;
+        //短信模版id
+        int tmplId = 73016;
+        //短信接收人
+        ArrayList<String> phoneNumbers = new ArrayList<String>(5);
+        phoneNumbers.add("13631785227");
+        //模版填充内容
+        ArrayList<String> params = new ArrayList<String>(5);
+        //会议主题
+        params.add("早会");
+        //距离会议开始时间
+        params.add("30");
+        //会议地点
+        params.add("会议室1");
+        //会议时间
+        params.add("2018-01-02 16:30");
+        //发送短信通知
+        try {
+        	smsMultiSenderResult = SmsUtil.sendMultiMessageWithParam(tmplId, params, phoneNumbers);
+            System.out.println(smsMultiSenderResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ src/test/java/cn/management/util/test/SmsUtilTest.java
@@ -0,0 +1,32 @@
+package cn.management.util.test;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import org.junit.Test;
+
+import cn.management.util.MailUtil;
+
+/**
+ * SmsUtil测试类
+ * @author ZhouJiaKai
+ * @date 2018-03-06
+ */
+public class SmsUtilTest {
+
+	@Test
+	public void sendMailTest() {
+		String toAddr = "598959863@qq.com";
+		String subject = "会议通知";
+		String content = "时间：2018-03-06 08:30 - 09:30，地点：会议室2";
+		try {
+			MailUtil.sendMail(toAddr, subject, content);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
