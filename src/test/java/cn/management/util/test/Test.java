11ee4e61d3ff20488257e14a0249c1e1a8ed6d66
ZhouJiaKai <zhoujk@pvc123.com>
2018-07-11 09:30:23 +0800

    test:test

---
+++ src/test/java/cn/management/util/test/Test.java
@@ -0,0 +1,19 @@
+package cn.management.util.test;
+
+/**
+ * <p>
+ * package: cn.management.util.test
+ * project: management
+ * </p>
+ *
+ * @author ZhouJiaKai <zhoujk@pvc123.com>
+ * @version v1.0.0
+ * @since v1.0.0
+ * <p>
+ * date 2018/7/11 9:29
+ */
+public class Test {
+
+    //test
+
+}
